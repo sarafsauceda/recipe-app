06ee4a32f03cdb54a3b243c09292f789d1a705e1
Sara Sauceda <sarafsauceda@gmail.com>
2022-03-13 14:02:22 -0700

    building pages

--- my-recipes/src/App.jsx
+++ my-recipes/src/App.jsx
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './pages/Home';
+import Pages from "./pages/Pages"
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Pages />
     </div>
   );
 }
---
+++ my-recipes/src/components/Veggie.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Veggie () {
+    return (
+      <div className="veggies">
+        Veggie
+      </div>
+    );
+  }
+  
+  export default Veggie;
