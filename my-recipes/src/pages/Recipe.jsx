4fd5e084414da60c4823c715cc6cd0e68153c323
Sara Sauceda <sarafsauceda@gmail.com>
2022-03-13 18:41:56 -0700

    still need to work on rendering images

--- my-recipes/src/pages/Recipe.jsx
+++ my-recipes/src/pages/Recipe.jsx
@@ -13,7 +13,7 @@ function Recipe() {
         const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`)
         const detailData = await data.json();
         setDetails(detailData);
-        console.log(detailData)
+
     };
 
     useEffect(() => {
