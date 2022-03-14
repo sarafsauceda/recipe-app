ea913c9916d02cad37ad3aa99dd2582f9fa7bbc5
Sara Sauceda <sarafsauceda@gmail.com>
2022-03-13 17:39:52 -0700

    recipe info hooked up

--- my-recipes/src/pages/Recipe.jsx
+++ my-recipes/src/pages/Recipe.jsx
@@ -4,11 +4,27 @@ import { useParams } from 'react-router-dom'
 import React from 'react'
 
 function Recipe() {
+
+    let params = useParams()
+    const [details, setDetails] = useState({})
+
+    const fetchDetails = async () => {
+        const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`)
+        const detailData = await data.json();
+        setDetails(detailData);
+    };
+
+    useEffect(() => {
+        fetchDetails();
+    }, [params.name])
+
     return (
       <div>
-          Recipes
+         {details.title}
       </div>
     );
   }
+
+  const DetailWrapper = 
   
   export default Recipe;
