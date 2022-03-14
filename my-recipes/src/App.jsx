5a838314fcfd2fff53a72afbcd0b696e50b266a6
Sara Sauceda <sarafsauceda@gmail.com>
2022-03-13 18:08:57 -0700

    need to add logo

--- my-recipes/src/App.jsx
+++ my-recipes/src/App.jsx
@@ -2,12 +2,19 @@ import Pages from "./pages/Pages"
 import Category from "./components/Category";
 import { BrowserRouter } from "react-router-dom";
 import Search from './components/Search'
+// import styled from "styled-components";
+// import Home from './pages.Home'
+// import {Link} from 'react'
 
 function App() {
   return (
     
     <div className="App">
       <BrowserRouter>
+      {/* <div><Link to={Home}>
+        What's for dinner?
+        </Link>
+        </div> */}
       <Search />
       <Category />
       <Pages />
@@ -16,5 +23,7 @@ function App() {
     
   );
 }
+
+
 
 export default App;
